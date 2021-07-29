32d9a3e5855880c159e26aa5c0f87f4ec0e97da6
mjavier1 <mjaguirre5@gmail.com>
2021-07-28 20:54:49 -0400

    where
    Userid

---
+++ creatadatabase/where.sql
@@ -0,0 +1,10 @@
+
+
+select UserId,frist_name,email
+from users
+where UserId =3 or email='adminhotmail.com';
+
+select UserId,frist_name,email
+from users
+where UserId = 1 and email='admin@hotmail.com';
+
