b957bbf88bb0ca4be2e2d9aecb330cef8eee9166
mjavier1 <mjaguirre5@gmail.com>
2021-07-28 20:35:55 -0400

    Update insert.sql

--- creatadatabase/insert.sql
+++ creatadatabase/insert.sql
@@ -1,2 +1,4 @@
 insert into  users(frist_name,last_name,email,pasword,is_admin,is_man,creaded_at) 
 values ('jonathan','castro','admin@hotmail.com',md5('1234'),1,'1',now());
+insert into  users(frist_name,last_name,email,pasword,is_admin,is_man,creaded_at) 
+values ('maria','castro','admin@hotmail.com',md5('1234'),2,'0',now());
