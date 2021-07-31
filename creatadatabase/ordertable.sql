76194c6f48f15250f41bdcb9ca7f14dcba26fa77
mjavier1 <mjaguirre5@gmail.com>
2021-07-31 19:41:20 -0400

    Create ordertable.sql

---
+++ creatadatabase/ordertable.sql
@@ -0,0 +1,24 @@
+
+use dev;
+
+
+/*
+
+
+
+
+select * from clients;
+
+
+
+
+alter table clients add column testin int (10) after phone
+alter table clients drop testin;
+
+insert into clients (nombre, addres, city, state, zip, phone)
+values ('mj','respactosabanetas','ponce','pr',00716,'787-472-6213');
+*/
+
+
+insert into clients (nombre, addres, city, state, zip, phone)
+values ('jr','sabanetas','ponce','pr',0010,'787-472-0000');
